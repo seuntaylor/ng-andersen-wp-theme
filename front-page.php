ed5ee7360f86c0cbec4efda895275744b62c1e3a
Oluseun Taylor <seuntaylor@gmail.com>
2026-04-11 17:50:33 +0100

    Add files to populate index.php

--- front-page.php
+++ front-page.php
@@ -2,7 +2,6 @@
 
     <main>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
             <?php the_content(); ?>
         <?php endwhile; endif; ?>
     </main>
